1d24f06c405527921362eb08d635cd92da37aec5
kevintone <kevinton@hawaii.edu>
2015-09-14 10:03:48 -1000

    w4c1

---
+++ IC/w4c1/jscript.js
@@ -0,0 +1,35 @@
+var getTime = "Get the current time:";
+document.getElementById("currentTime").onclick = getCurrentTime;
+
+function getCurrentTime(){
+    document.getElementById("funcTime").innerHTML=Date();
+}
+
+function mouseDown(obj) {
+    obj.style.backgroundColor = "#1ec5e5";
+    obj.innerHTML = "Thnaks";
+}
+
+function mouseUp(obj) {
+    obj.style.backgroundColor ="#D94A38";
+
+    obj.innerHTML = "Click Me :)";
+}
+
+function enlargeImage(obj) {
+    obj.style.width = "200px";
+    obj.style.height ="200px";
+}
+
+function normalImage(obj) {
+    obj.style.width = "100px";
+    obj.style.height = "100px";
+}
+
+
+document.getElementById("greet").onclick = getGreeting;
+
+function getGreeting() {
+    var greeting = "Have A Great Day :)"
+    docutment.getElementById("theGreeting").innerHTML=print(greeting);
+}
